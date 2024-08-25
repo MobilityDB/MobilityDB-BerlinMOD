f93bcb6eae414dfe71b028b896008be898d882b9
Maxime Schoemans <maxime.schoemans@ulb.ac.be>
2024-08-25 18:01:45 +0200

    Simplify runall script

--- BerlinMOD/berlinmod_runall.sh
+++ BerlinMOD/berlinmod_runall.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 # A script to run the BerlinMOD generator
+# Run this script inside the BerlinMOD/ folder.
+# This script expects the file brussels.osm
+# to be in the BerlinMOD/ folder.
 
+# Moodify these configs as needed
 host=localhost
-port=5433
-dbowner=mahmoud
-database=brussels
-datapath=~/Desktop/MobilityDB/MobilityDB_BeforeNewGit/BerlinMOD/data/
-scriptpath=~/Desktop/MobilityDB/MobilityDB-BerlinMOD/BerlinMOD/
+port=5432
+dbowner=postgres
+database=berlinmod
 scalefactor=0.005
 
 dropdb -h $host -p $port -U $dbowner $database
 createdb -h $host -p $port -U $dbowner $database
-#psql -h $host -p $port -U $dbowner -d $database -c 'CREATE EXTENSION MobilityDB CASCADE'
-#psql -h $host -p $port -U $dbowner -d $database -c 'CREATE EXTENSION pgRouting'
-#osm2pgrouting -h $host -p $port -U $dbowner -f $datapath/brussels.osm --dbname $database -c mapconfig_$database.xml
-#osm2pgsql -c -H $host -P $port -U $dbowner -d $database $datapath/brussels.osm
-#pg_dump -h localhost -p 5433 -U mahmoud -W -F t brussels > brussels.tar
-pg_restore --dbname=$database --create --verbose $datapath/brussels.tar
-psql -h $host -p $port -U $dbowner -d $database -f $scriptpath/brussels_preparedata.sql
-psql -h $host -p $port -U $dbowner -d $database -f $scriptpath/berlinmod_datagenerator_batch.sql
-psql -h $host -p $port -U $dbowner -d $database -c 'select berlinmod_generate(scaleFactor := '$scalefactor')'
-psql -h $host -p $port -U $dbowner -d $database -f $scriptpath/tests.sql
+
+psql -h $host -p $port -U $dbowner -d $database -c 'CREATE EXTENSION MobilityDB CASCADE'
+psql -h $host -p $port -U $dbowner -d $database -c 'CREATE EXTENSION pgRouting'
+osm2pgrouting -h $host -p $port -U $dbowner -f ./brussels.osm --dbname $database -c mapconfig.xml
+osm2pgsql -c -H $host -P $port -U $dbowner -d $database ./brussels.osm
+
+# pg_dump -h localhost -p 5433 -U $dbowner -W -F t brussels > brussels.tar
+# pg_restore --dbname=$database --create --verbose ./brussels.tar
+
+psql -h $host -p $port -U $dbowner -d $database -f ./brussels_preparedata.sql
+psql -h $host -p $port -U $dbowner -d $database -f ./berlinmod_datagenerator.sql
+psql -h $host -p $port -U $dbowner -d $database -c 'select berlinmod_datagenerator(scaleFactor := '$scalefactor')'
+# psql -h $host -p $port -U $dbowner -d $database -f ./tests.sql
